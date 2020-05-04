5d4c733116d2a771415da4eb03679238564424d6
AuroraTwinkle <duanlinlin66@163.com>
2020-05-04 12:20:05 +0800

    init the 'understanding the JVM' project and add .gitignore file.

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+    public static Main SAVE_HOOK =null;
+    public void isAlive(){
+        System.out.println("yes,i am still alive.");
+    }
+    @Override
+    protected  void finalize() throws Throwable{
+        super.finalize();
+        System.out.println("finalize method executed.");
+        Main.SAVE_HOOK=this;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+        SAVE_HOOK=new Main();
+        SAVE_HOOK=null;
+        System.gc();
+        Thread.sleep(500);//stop to wait for Finalize method.
+        if(SAVE_HOOK!=null){
+            SAVE_HOOK.isAlive();
+        }else {
+            System.out.println("no,i am dead.");
+        }
+        SAVE_HOOK=null;
+        System.gc();
+        Thread.sleep(500);
+        if(SAVE_HOOK!=null){
+            SAVE_HOOK.isAlive();
+        }else {
+            System.out.println("no,i am dead.");
+        }
+    }
+}
